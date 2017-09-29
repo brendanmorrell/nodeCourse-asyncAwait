dd3006e771f1302dab3c39264022b01514679ca0
Brendan Morrell <brendanmorrell@gmail.com>
2017-09-29 12:17:53 -0600

    added error handling and toUpperCase for currency converter

--- currency-convert.js
+++ currency-convert.js
@@ -1,36 +1,40 @@
 const axios = require('axios')
 
-const getExchangeRate = (from, to) => {
-  return axios.get(`http://api.fixer.io/latest?base=${from}`).then((response) => {
+const getExchangeRate = async (from, to) => {
+  try {
+    const response = await axios.get(`http://api.fixer.io/latest?base=${from}`);
+  } catch (e) {
+    throw new Error (`Server request unable to complete using currecy codes ${from} and ${to}`)
+  }
+  try {
     return response.data.rates[to];
-  });
+  } catch (e) {
+    throw new Error (`Connected to server, but no exchange data found between ${from} and ${to}`);
+  }
 };
 
-const getCountries = (currency) => {
-  return axios.get(`https://restcountries.eu/rest/v2/currency/${currency}`).then((response) => {
+const getCountries = async (currencyCode) => {
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
     return response.data.map((country) => country.name);
-  });
+  } catch (e) {
+    throw new Error (`No countries found matching the currency code ${currencyCode}`)
+  }
 };
 
-/*const convertCurrency = (from, to, amount) => {
-  let countries;
-  return getCountries(to).then((tempCountries) => {
-    countries=tempCountries;
-    return getExchangeRate(from, to);
-  }).then((rate) => {
-    const exchangedAmount = amount* rate;
-    return `${amount} ${from} is worth ${exchangedAmount} in ${to}. ${to} can be used in the following countries:${countries.join(', ')}`
-  });
-};*/
 
 const convertCurrency = async (from, to, amount) => {
-  const countries = await getCountries(to);
-  const exchangeRate = await getExchangeRate(from, to);
+  let upperFrom= from.toUpperCase();
+  let upperTo= to.toUpperCase();
+  const countries = await getCountries(upperTo);
+  const exchangeRate = await getExchangeRate(upperFrom, upperTo);
   const exchangedAmount = amount*exchangeRate;
 
-  return `${amount} in ${from} is equal to ${exchangedAmount} in ${to}. ${to} can be used in the folloeing countries: ${countries.join(', ')}`
+  return `${amount} in ${upperFrom} is equal to ${exchangedAmount} in ${upperTo}. ${upperTo} can be used in the following countries: ${countries.join(', ')}`
 };
 
-convertCurrency('CAD', 'USD', 100).then((string) => {
+convertCurrency('usd', 'usd', 200).then((string) => {
   console.log(string);
+}).catch((e) => {
+  console.log(e.message);
 });
